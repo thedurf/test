9e55a85e03d72b91ebaf2850c326bc00fdafbee2
Brian Stephens <brian.stephens@ktech.com>
2018-12-05 18:27:00 +0000

    Create ping-fileTester.ps1

---
+++ ping-fileTester.ps1
@@ -0,0 +1,10 @@
+﻿$names = Get-content "servers_active.txt"
+
+foreach ($name in $names){
+  if (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
+    Write-Host "$name,up" -ForegroundColor Green;
+  }
+  else{
+    Write-Host "$name,down" -ForegroundColor Red;
+  }
+}
