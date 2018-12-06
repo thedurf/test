287cd5f6f1d9dc098034500b095881d4c2e9268e
thedurf <thedurf@gmail.com>
2018-12-06 21:08:07 +0000

    New commit

---
+++ PS_Headers.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+Script to get local admin password for a PC. Must be run with elevated permissions.
+
+.DESCRIPTION
+08/02/2018 get-LapsPasswords.psl
+
+.PARAMETER computerNameAccepts 
+Pass the name of the PC to Local Administrator Password
+
+.EXAMPLE
+get-LapsPasswords.psl <HostName>
+
+.NOTES
+Needs to be to with elevated permissions,  Administrator data can be collected from Active Directory.
+
+.LINK
+#>
